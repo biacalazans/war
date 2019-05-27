fe3365c8460526497363fac2ea518385bc812839
JojiG <30815887+JojiG@users.noreply.github.com>
2019-05-27 09:40:30 -0300

    Add files via upload

---
+++ Regras.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("WAR\n");
+	printf("Objetivo:\n");
+	printf("Destruir todos os exercitos do inimigo.\n");
+	printf("Regras:\n");
+	printf("1-Em cada rodada o jogador recebera mais exercitos, que devem ser distribuidos em seus paises.\n");
+	printf("2-O jogador pode atacar quantas vezes ele quiser ate ele desistir ou ficar sem exercitos para atacar.\n");
+	printf("3-O jogador pode atacar os paises vizinhos ao de origem do ataque.\n");
+	printf("4-O jogador de ataque e de defesa farao uma rolada de dados e o que tiver a menor soma perde 1 unidade do exercito.\n");
+	printf("5-Quando o exercito de uma regiao chegar a zero o atacante pode alocar suas unidades para la.\n");
+	printf("6-Ao conquistar todas as regioes o jogo acaba.\n");
+	
+	return 0;
+}
