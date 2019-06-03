350a5906506b881a2dc990c9e7de084ca1a8c96c
JojiG <30815887+JojiG@users.noreply.github.com>
2019-06-03 09:08:44 -0300

    Update Regras.c

--- Regras.c
+++ Regras.c
@@ -9,7 +9,7 @@ int main()
 	printf("1-Em cada rodada o jogador recebera mais exercitos, que devem ser distribuidos em seus paises.\n");
 	printf("2-O jogador pode atacar quantas vezes ele quiser ate ele desistir ou ficar sem exercitos para atacar.\n");
 	printf("3-O jogador pode atacar os paises vizinhos ao de origem do ataque.\n");
-	printf("4-O jogador de ataque e de defesa farao uma rolada de dados.\n");
+	printf("4-O jogador de ataque e de defesa farao uma rolada de dados e quem tirar o menor valor perdera uma tropa.\n");
 	printf("5-Quando o exercito de uma regiao chegar a zero o atacante pode alocar suas unidades para la.\n");
 	printf("6-Ao conquistar todas as regioes o jogo acaba.\n");
 	
